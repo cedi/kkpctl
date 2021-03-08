853faf1441063b6a45b37072b277cc45e8c1ce10
Cedric Kienzler <cedric.kienzler@innovo-cloud.de>
2021-03-08 18:43:52 +0100

    fix main

--- main.go
+++ main.go
@@ -6,27 +6,14 @@ import (
 	kkpconfig "github.com/cedi/kkpctl/pkg/config"
 )
 
+// Config is the global configuration for the kkpctl
+var Config *kkpconfig.Config
+
 func main() {
-
-	config := kkpconfig.New()
-	config.Clouds = append(config.Clouds, kkpconfig.Cloud{
-		Name: "test",
-		URL:  "imke.cloud",
-	})
-
-	config.BearerToken = "test123"
-	config.Context = kkpconfig.Context{
-		Cloud:     "test",
-		ProjectID: "x12vas",
-	}
-
-	config.Save()
-
-	config2, err := kkpconfig.ReadFromConfig("/Users/cedi/.config/kkpctl/config")
+	var err error
+	Config, err = kkpconfig.ReadFromConfig("/Users/cedi/.config/kkpctl/config")
 	if err != nil {
 		fmt.Println("Error: " + err.Error())
 		return
 	}
-
-	fmt.Printf("%v\n", config2)
 }
