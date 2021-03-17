503b2c658a6f0249627791c003cdd0d10caecc72
Cedric Kienzler <cedric.kienzler@innovo-cloud.de>
2021-03-17 12:06:54 +0000

    remove bearer from context

--- pkg/config/context.go
+++ pkg/config/context.go
@@ -3,13 +3,11 @@ package config
 // Context holds the current context of which cloud to use
 type Context struct {
 	CloudName string `yaml:"cloud"`
-	Bearer    string `yaml:"bearer"`
 }
 
 // NewContext creates a empty context for kkpctl
 func NewContext() Context {
 	return Context{
 		CloudName: "",
-		Bearer:    "",
 	}
 }
