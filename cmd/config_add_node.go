ac5e9b05cb55c22ea96f5b64d4cf5738d0046da4
Cedric Kienzler <cedric.kienzler@innovo-cloud.de>
2021-04-28 09:26:46 +0000

    fix a bug with openstack to correctly set instanceReadyCheck(period|timeout)

--- cmd/config_add_node.go
+++ cmd/config_add_node.go
@@ -8,9 +8,11 @@ import (
 )
 
 var (
-	flavor        string
-	image         string
-	useFloatingIP bool
+	flavor                    string
+	image                     string
+	useFloatingIP             bool
+	instanceReadyCheckPeriod  string
+	instanceReadyCheckTimeout string
 )
 
 var configAddNodeSpecCmd = &cobra.Command{
@@ -28,9 +30,11 @@ var configAddOSNodeSpecCmd = &cobra.Command{
 		name := args[0]
 
 		err := Config.NodeSpec.AddCloudNodeSpec(name, &models.OpenstackNodeSpec{
-			Flavor:        &flavor,
-			Image:         &image,
-			UseFloatingIP: useFloatingIP,
+			Flavor:                    &flavor,
+			Image:                     &image,
+			UseFloatingIP:             useFloatingIP,
+			InstanceReadyCheckPeriod:  instanceReadyCheckPeriod,
+			InstanceReadyCheckTimeout: instanceReadyCheckTimeout,
 		})
 
 		if err != nil {
@@ -55,5 +59,7 @@ func init() {
 	configAddOSNodeSpecCmd.MarkFlagRequired("image")
 
 	configAddOSNodeSpecCmd.Flags().BoolVar(&useFloatingIP, "floating_ip", false, "Allocate a floating IP")
-	configAddOSNodeSpecCmd.MarkFlagRequired("image")
+
+	configAddOSNodeSpecCmd.Flags().StringVar(&instanceReadyCheckPeriod, "instance_ready_check_period", "10s", "interval in which the instance readyness is checked")
+	configAddOSNodeSpecCmd.Flags().StringVar(&instanceReadyCheckTimeout, "instance_ready_check_timeout", "5m", "timeout how long to wait for a instance")
 }
